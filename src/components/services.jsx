2f4c26f9f625acda3317f850c361718246c6eed1
iFrogHop2Worlds <bbest12g@gmail.com>
2022-10-14 18:53:59 -0700

    small updated to the service page

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -4,10 +4,22 @@ export default function Services() {
   return (
     <div id='background'>
      <div class="row">
-      <div class="column">Websites/Applications</div>
-      <div class="column">Update Codebase</div>
-      <div class="column">Security Audit</div>
-      <div class="column">Consultation</div>
+      <div class="column">
+        <h4>Websites/Applications</h4>
+        <p>Get a custom website or application purpose built for your needs</p>
+      </div>
+      <div class="column">
+        <h4>Update Codebase</h4>
+        <p>Have an old application and want to update those dependencies but worried about breaking things? I got you. I will update and refactor your code as required to ensure a smooth transition</p>
+      </div>
+      <div class="column">
+        <h4>Security Audit</h4>
+        <p>penetration testing and report</p>
+      </div>
+      <div class="column">
+        <h4>Consultation</h4>
+        <p>come to me with your problems and we'll figure something out.</p>
+      </div>
     </div> 
 
 
